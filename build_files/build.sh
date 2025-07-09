1789d8e05057ca05c67e931c0630487876f41b77
Alexandre de Siqueira <5776022+alexdesiqueira@users.noreply.github.com>
2025-07-09 12:40:18 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf install -y tmux
+dnf install sway-config-fedora greetd tuigreet @sway-extended
 
 # Use a COPR Example:
 #
@@ -22,3 +23,5 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl disable sddm
+systemctl enable greetd
