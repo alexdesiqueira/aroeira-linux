72edbfae8233cbb6653f47b1a934b2253bc131e1
Alexandre de Siqueira <5776022+alexdesiqueira@users.noreply.github.com>
2025-08-08 21:53:44 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y sway-config-fedora @swaywm-extended evince gammastep gnome-disk-utility syncthing udiskie
+dnf install -y sway-config-fedora @swaywm-extended evince gammastep gnome-disk-utility simple-scan syncthing transmission udiskie
 
 # Use a COPR Example:
 #
